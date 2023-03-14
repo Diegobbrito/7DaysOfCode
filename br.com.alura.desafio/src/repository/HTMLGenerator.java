d8907140e5ce1de0cce11da758d1ae215e26aa4b
Diego Brito <diego.brito@f1rst.com.br>
2023-03-13 21:46:45 -0300

    Atualizada a api para imdb e o gerador de html

--- br.com.alura.desafio/src/repository/HTMLGenerator.java
+++ br.com.alura.desafio/src/repository/HTMLGenerator.java
@@ -1,6 +1,6 @@
 package repository;
 
-import dto.Movies;
+import dto.Content;
 
 import java.io.IOException;
 import java.io.Writer;
@@ -13,17 +13,17 @@ public class HTMLGenerator {
         this.writer = writer;
     }
 
-    public void generate(List<Movies> movies) throws IOException {
+    public void generate(List<? extends Content> movies) throws IOException {
 
         writer.write("<head><meta charset=uft-8></head>");
         writer.write("<body>");
 
         movies.forEach(m -> {
             try {
-                String image =  m.getUrl();
-                int ano = m.getAno();
-                String nota = m.getNota();
-                writer.write("<h2>"+m.getTitulo()+"</h2>");
+                String image =  m.urlImage();
+                String ano = m.year();
+                String nota = m.rating();
+                writer.write("<h2>"+m.title()+"</h2>");
                 writer.write("<br><img src=" +image + " width=\"450\" height=\"600\"><br>");
                 writer.write("Ano : " + ano+" Nota : "+nota + "<br><br>");
             } catch (IOException e) {
--- br.com.alura.desafio/src/repository/ImdbApiClient.java
+++ br.com.alura.desafio/src/repository/ImdbApiClient.java
@@ -1,11 +1,13 @@
 package repository;
 
+import dto.APIClient;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-public class ImdbApiClient {
+public class ImdbApiClient implements APIClient {
 
     private final HttpClient client;
 
@@ -13,14 +15,19 @@ public class ImdbApiClient {
         this.client = HttpClient.newHttpClient();
     }
 
-    public String getImdbTop250(){
+    @Override
+    public String getBody() {
         try{
             HttpRequest request = HttpRequest.newBuilder().uri(new URI("https://imdb-api.com/en/API/Top250Movies/"))
-                .GET().build();
+                    .GET().build();
             return client.send(request, HttpResponse.BodyHandlers.ofString()).body();
         } catch (Exception e){
             return null;
         }
     }
 
+    @Override
+    public Class type() {
+        return null;
+    }
 }
